aee029fa21efffbadec3b4c08d96556c34c89eec
Colleen O'Rourke <colleen@sentry.io>
2020-07-10 12:06:24 -0700

    this probably will change nothing

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,10 @@ import * as Sentry from '@sentry/node'
 process.env.NODE_ENV === "production" &&
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    debug: true,
+    environment: process.env.NODE_ENV
   });
-  
+
 export default function App({ Component, pageProps, err }) {
   // Workaround for https://github.com/vercel/next.js/issues/8592
   return <Component {...pageProps} err={err} />
