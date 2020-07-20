8bcfa5d93d9aff4d4d235f2bdd7adecf61519e7e
Colleen O'Rourke <colleen@sentry.io>
2020-07-20 12:45:54 -0700

    boop

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ import * as Sentry from '@sentry/node'
 
 process.env.NEXT_PUBLIC_SENTRY_DSN &&
   Sentry.init({
-    enabled: process.env.ENV === "production",
+    enabled: process.env.NODE_ENV === "production",
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   });
   
